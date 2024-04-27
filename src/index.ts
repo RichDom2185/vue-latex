275de3825a263d88d4fc75b8ba7c602355e97a83
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-04-27 23:47:10 +0800

    Create initial version

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import katex, { KatexOptions } from "katex";
+import type { Plugin } from "vue";
+
+type Options = {
+  /**
+   * The Provider to use for rendering the LaTeX expression.
+   * Currently, only "katex" is supported.
+   * @default "katex"
+   */
+  provider?: "katex";
+  /**
+   * Global options to be passed to the KaTeX render method.
+   * @see https://katex.org/docs/options.html
+   */
+  globalOptions?: KatexOptions;
+};
+
+const VueLatex: Plugin<[Options | undefined]> = {
+  install(app, options = {}) {
+    // Inject the $latex method into the Vue app.
+    app.config.globalProperties.$latex = (expression: string) => {
+      return katex.renderToString(expression, {
+        throwOnError: false,
+        displayMode: false, // Inline mode by default
+        ...options.globalOptions,
+      });
+    };
+  },
+};
+
+export default VueLatex;
